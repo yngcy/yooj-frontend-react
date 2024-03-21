88746b82ffd823434b71c3132d0d89d1dd12c97e
youngccy <2792783811@qq.com>
2024-03-21 18:43:47 +0800

    feat: 添加系统管理模块

---
+++ src/pages/admin/System/index.tsx
@@ -0,0 +1,12 @@
+import { PageContainer } from '@ant-design/pro-components';
+import React from 'react';
+
+const SystemPage: React.FC = () => {
+  return (
+    <>
+      <PageContainer header={{ title: '系统管理' }}></PageContainer>
+    </>
+  );
+};
+
+export default SystemPage;
