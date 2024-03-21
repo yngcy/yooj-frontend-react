00aea4f4321b4d9fa8d9dd01dde75cc0dfaabddb
youngccy <2792783811@qq.com>
2024-03-21 18:44:02 +0800

    feat: 初始化系统标签管理模块

---
+++ src/pages/admin/Tags/index.tsx
@@ -0,0 +1,12 @@
+import { PageContainer } from '@ant-design/pro-components';
+import React from 'react';
+
+const TagsPage: React.FC = () => {
+  return (
+    <>
+      <PageContainer header={{ title: '标签管理' }}></PageContainer>
+    </>
+  );
+};
+
+export default TagsPage;
