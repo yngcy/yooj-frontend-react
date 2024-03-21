2f5fbc0ef3e656184e98c493235f7526a2435a90
youngccy <2792783811@qq.com>
2024-03-21 18:37:45 +0800

    feat: 添加图标映射

---
+++ src/utils/IconMap.tsx
@@ -0,0 +1,21 @@
+import {
+  AlertOutlined,
+  ClusterOutlined,
+  CoffeeOutlined,
+  DashboardOutlined,
+  InboxOutlined,
+  TagsOutlined,
+  TeamOutlined,
+  TrophyOutlined,
+} from '@ant-design/icons';
+
+export const IconMap = {
+  DashboardOutlined: <DashboardOutlined />,
+  ClusterOutlined: <ClusterOutlined />,
+  TeamOutlined: <TeamOutlined />,
+  AlertOutlined: <AlertOutlined />,
+  InboxOutlined: <InboxOutlined />,
+  TrophyOutlined: <TrophyOutlined />,
+  CoffeeOutlined: <CoffeeOutlined />,
+  TagsOutlined: <TagsOutlined />,
+};
