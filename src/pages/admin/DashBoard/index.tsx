5837b81c6971b56c4a60dfe696b984e777956c6b
youngccy <2792783811@qq.com>
2024-03-21 18:42:21 +0800

    feat: 初始化仪表盘模块

---
+++ src/pages/admin/DashBoard/index.tsx
@@ -0,0 +1,12 @@
+import { PageContainer } from '@ant-design/pro-components';
+import React from 'react';
+
+const DashBoardPage: React.FC = () => {
+  return (
+    <>
+      <PageContainer header={{ title: '仪表盘' }}></PageContainer>
+    </>
+  );
+};
+
+export default DashBoardPage;
