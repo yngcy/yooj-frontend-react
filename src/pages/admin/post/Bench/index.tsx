4927f3de378ca67c9b7b1f4c022ff0c11cdf8ad5
youngccy <2792783811@qq.com>
2024-03-21 18:42:47 +0800

    feat: 初始化帖子管理模块

---
+++ src/pages/admin/post/Bench/index.tsx
@@ -0,0 +1,12 @@
+import { PageContainer } from '@ant-design/pro-components';
+import React from 'react';
+
+const BenchPage: React.FC = () => {
+  return (
+    <>
+      <PageContainer header={{ title: '帖子工作台' }}></PageContainer>
+    </>
+  );
+};
+
+export default BenchPage;
---
+++ src/pages/admin/post/Config/index.tsx
@@ -0,0 +1,12 @@
+import { PageContainer } from '@ant-design/pro-components';
+import React from 'react';
+
+const ConfigPage: React.FC = () => {
+  return (
+    <>
+      <PageContainer header={{ title: '帖子配置' }}></PageContainer>
+    </>
+  );
+};
+
+export default ConfigPage;
