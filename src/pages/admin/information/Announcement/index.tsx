f8af7618a72dd4301e6f70f6bf2a77ba94810924
youngccy <2792783811@qq.com>
2024-03-21 18:42:37 +0800

    feat: 初始化消息通知模块

---
+++ src/pages/admin/information/Announcement/index.tsx
@@ -0,0 +1,12 @@
+import { PageContainer } from '@ant-design/pro-components';
+import React from 'react';
+
+const AnnouncementPage: React.FC = () => {
+  return (
+    <>
+      <PageContainer header={{ title: '公告管理' }}></PageContainer>
+    </>
+  );
+};
+
+export default AnnouncementPage;
---
+++ src/pages/admin/information/Notice/index.tsx
@@ -0,0 +1,12 @@
+import { PageContainer } from '@ant-design/pro-components';
+import React from 'react';
+
+const NoticePage: React.FC = () => {
+  return (
+    <>
+      <PageContainer header={{ title: '通知管理' }}></PageContainer>
+    </>
+  );
+};
+
+export default NoticePage;
